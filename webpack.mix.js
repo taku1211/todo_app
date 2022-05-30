dcdf207de1836254394a9fbb3838039524e40731
Takuma Kanazawa <eennyade.1211.t@gmail.com>
2022-05-30 21:20:03 +0900

    SPA environment created

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
