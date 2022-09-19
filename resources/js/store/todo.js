ce6689c230dbd63ce375478c6a2a416b8a8b4feb
Takuma Kanazawa <eennyade.1211.t@gmail.com>
2022-09-19 22:16:28 +0900

    todo.js change

--- resources/js/store/todo.js
+++ resources/js/store/todo.js
@@ -86,6 +86,8 @@ const actions = {
             const m = ("00" + (dt.getMonth()+1)).slice(-2);
             const d = ("00" + dt.getDate()).slice(-2);
             const today = y + "-" + m +"-"+ d;
+
+            console.log(response)
             
         if(response.status === OK){
             context.commit('setApiStatus', true)
